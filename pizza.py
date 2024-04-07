742a7234b04faf6df58fe40f9b61e78c6a602547
diamondandy <dandy@terpmail.umd.edu>
2024-04-06 20:28:42 -0400

    docstrings
    unit test not complete yet :P

--- pizza.py
+++ pizza.py
@@ -1,3 +1,22 @@
-print("Hello World")
-print("goodbye")
-print("Hiiiiiiiii")
+#class Pizza
+
+'''This class will have the pizza size, toppings, and the cost of them'''
+
+#def init
+    #self.size of the pizza
+    #self.toppings for the pizza
+    
+#def total_cost
+    #there will be a base cost for the pizza (around 9.99)
+    #toppings_cost (about 1.00 or 1.50 for each topping)
+    
+#def order
+    #a string that will return the order of the customer
+    
+#unit test
+
+def toppings():
+    
+    pizza_topping = input("Enter what topping you would like")
+    
+    
